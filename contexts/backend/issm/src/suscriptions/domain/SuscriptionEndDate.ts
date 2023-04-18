83ad259b87c028e4c8a41edc1403f62110991c73
Raúl Fernández Fernández <raulparri71@gmail.com>
2023-04-18 14:21:49 +0200

    chore: remove unused methods
    Signed-off-by: Raúl Fernández Fernández <raulparri71@gmail.com>

--- contexts/backend/issm/src/suscriptions/domain/SuscriptionEndDate.ts
+++ contexts/backend/issm/src/suscriptions/domain/SuscriptionEndDate.ts
@@ -1,14 +1,3 @@
 import { DateValueObject } from "../../shared/domain/value-object/DateValueObject";
-import { InvalidArgumentError } from "../../shared/domain/value-object/InvalidArgumentError";
 
-export class SuscriptionEndDate extends DateValueObject {
-	static fromValue(value: string): SuscriptionEndDate {
-		const date = new Date(value);
-
-		if (isNaN(date.getTime())) {
-			throw new InvalidArgumentError(`The value <${value}> is not a valid date`);
-		}
-
-		return new SuscriptionEndDate(date);
-	}
-}
+export class SuscriptionEndDate extends DateValueObject {}
--- contexts/backend/issm/src/suscriptions/domain/SuscriptionStartDate.ts
+++ contexts/backend/issm/src/suscriptions/domain/SuscriptionStartDate.ts
@@ -1,14 +1,3 @@
 import { DateValueObject } from "../../shared/domain/value-object/DateValueObject";
-import { InvalidArgumentError } from "../../shared/domain/value-object/InvalidArgumentError";
 
-export class SuscriptionStartDate extends DateValueObject {
-	static fromValue(value: string): SuscriptionStartDate {
-		const date = new Date(value);
-
-		if (isNaN(date.getTime())) {
-			throw new InvalidArgumentError(`The value <${value}> is not a valid date`);
-		}
-
-		return new SuscriptionStartDate(date);
-	}
-}
+export class SuscriptionStartDate extends DateValueObject {}
